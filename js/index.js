e68a2afa4ba38518b8ce9b06eb230d72689d1aee
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2018-06-09 04:49:42 +0200

    Add map to site

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 'use strict';
 
 (function() {
-/////
 
 var galleryTemplate = document.getElementById('gallery-template').innerHTML;
 var galleryList = '';
 for(var i = 0; i < galleryData.length; i++){
 		galleryList += Mustache.render(galleryTemplate, galleryData[i]);
 	}
-	console.log(galleryData);
-	console.log(galleryList);
+
 var carouselMain = document.getElementById('carousel-main');
 carouselMain.insertAdjacentHTML('beforeEnd', galleryList);
-/////
+
 var carousel = document.querySelector('.carousel');
 var flkty = new Flickity( carousel, {
   hash: true, wrapAround: true
@@ -34,6 +32,26 @@ buttonGroup.addEventListener( 'click', function( event ) {
   var selector = event.target.getAttribute('data-selector');
   flkty.selectCell( selector );
 });
+
+window.initMap = function() {
+	var warszawa = {lat: 52.231593, lng: 21.019990};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: warszawa
+		});
+
+	galleryData.forEach(function (dataElement) {
+		var marker = new google.maps.Marker({
+			position: dataElement.coords,
+			map: map
+		})
+		var title = dataElement.title;
+		marker.addListener('click', function(dataElement){
+			infos.innerHTML = 'You clicked marker ' + title;
+		})
+	});
+
+	}
 
 
   })();
